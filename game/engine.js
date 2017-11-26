ad9bf6798e2b18c69ade9d551c2fe8d685a7dacf
GaiusCoffee <gaius.coffee@gmail.com>
2017-11-26 22:51:57 +0800

    Continue working on game html boilerplate.

--- game/engine.js
+++ game/engine.js
@@ -2,9 +2,12 @@ class engine {
     constructor(){
         $("#btnTest").click(() => {this.c("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")});
         $("#console").empty();
-        for(let i=0;i<10;i++) {
+        for(let i=0;i<20;i++) {
             $("#console").append($("<li>"));
         }
+        $('#consoleWrapper').stop().animate({
+            scrollTop: $('#consoleWrapper')[0].scrollHeight
+        }, 800);
     }
     c(message){
         $('#console li').first().remove();
